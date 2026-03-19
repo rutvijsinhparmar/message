aef283bba7739b920753e8e06d7a67c8d4afb2f0
Rutvijsinh Parmar <infobhologujju@gmail.com>
2026-03-19 13:08:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const slide1 = document.getElementById('slide1');
 const slide2 = document.getElementById('slide2');
 
 startBtn.addEventListener('click', () => {
+    // Fade out Slide 1
     slide1.style.opacity = '0';
+    
     setTimeout(() => {
         slide1.style.display = 'none';
-        slide2.classList.add('show');
+        // Show Slide 2
+        slide2.classList.add('active-slide');
     }, 800);
 });
