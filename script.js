8b9ea6b43fb2eb4e15f2eeba052068154f0fe30a
Rutvijsinh Parmar <infobhologujju@gmail.com>
2026-03-19 12:41:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ const slide1 = document.getElementById('slide1');
 const slide2 = document.getElementById('slide2');
 
 startBtn.addEventListener('click', () => {
-    // Hide slide 1
     slide1.classList.add('hide');
     
-    // After a short delay, show slide 2
     setTimeout(() => {
         slide1.style.display = 'none';
         slide2.classList.add('show');
