06c3dbbf3bdb601a66d2af3093ad083ffb2c96e1
Rutvijsinh Parmar <infobhologujju@gmail.com>
2026-03-19 13:04:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ const slide1 = document.getElementById('slide1');
 const slide2 = document.getElementById('slide2');
 
 startBtn.addEventListener('click', () => {
-    // Fade out first slide
-    slide1.classList.add('hide');
-    
-    // Smooth transition to bouquet
+    slide1.style.opacity = '0';
     setTimeout(() => {
         slide1.style.display = 'none';
         slide2.classList.add('show');
